c3588205c128bdec7bbfb3bda1977af584c50191
DELL <niheleeroui124@gmail.com>
2026-05-21 16:18:17 +0200

    chore: configure database connection

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+//importer la librairie mongoose pour se connecter à la base de données MongoDB
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+    } catch {
+        console.log(error);
+        process.exit(1); //arreter complètement l'application
+
+    }
+};
+
+module.exports = connectDB;
