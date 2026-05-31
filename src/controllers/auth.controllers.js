a9bb44a6c73032b5ebae179e6ba77e6f9afa63c8
DELL <niheleeroui124@gmail.com>
2026-05-31 16:25:23 +0200

    feat: add login functionality with route

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -25,3 +25,40 @@ exports.register = async (req , res) => {
 
     }
 };
+
+exports.login = async (req, res) => {
+
+    try {
+        const {email,password } = req.body;
+    //check user
+    const user = await User.findOne({email});
+    if (!user){
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+    //check password
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if(!matchPassword){
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+    //generate token
+    const token = jwt.sign(
+        { id : user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
+    );
+
+    res.json({
+        message: "Login successful",
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        },
+    });
+    } catch ( error){
+        res.status(500).json({ message: error.message });
+    }
+
+};
